c8e439318e907c7511d257843f33a5e785619195
Alan Izaguirre <alanizaguirre07819@gmail.com>
2023-07-05 07:23:38 -0700

    mathquiz.py

---
+++ sheet2/mathquiz.py
@@ -0,0 +1,33 @@
+from random import randint #
+randint(10, 15)
+13
+num1 = randint(1, 10)
+num2 = randint(1, 10)
+num1
+9
+num2
+1
+num1 = 9
+num2 = 1
+question = "What is" + str(num1) + " times " + str(num2) + "? "
+print(question)
+What is 9 times 1? 
+print(int(input("Enter a number: ")))
+Enter a number: 9
+9
+print(3 * int(input("Enter a number: ")))
+Enter a number: 9
+27
+print(int(input("What is 5 time 9? ")))
+What is 5 time 9? 45
+45
+question = 'What is your favorite number? '
+answer = int(input(question))
+What is your favorite number? 7
+answer
+7
+if 1 < 2:
+    print('Something is wrong!')
+else:
+    print('Something is right!')
+if 1>1000:
